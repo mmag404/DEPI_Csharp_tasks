fb0bc53233194dd50f30df96e55f425ff572d15f
magdy <mmag8055@gmail.com>
2026-03-06 19:47:08 +0200

    done

---
+++ Task-11/day03/Contexts/BookstoreDbContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using day03.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace day03.Contexts
+{
+    internal class BookstoreDbContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=BookstoreDB;Trusted_Connection=True;TrustServerCertificate=True;");
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>(entity =>
+            {
+                entity.Property(b => b.Title)
+                      .IsRequired()
+                      .HasMaxLength(150);
+
+                entity.Property(b => b.Price)
+                      .HasColumnType("decimal(8,2)");
+
+                entity.Property(b => b.PublishedDate)
+                      .IsRequired(false);
+            });
+        }
+
+        public DbSet<Author>Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+    }
+}
---
+++ Task-11/day03/Models/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day03.Models
+{
+
+    // Q1: Why did the property "Id" become a Primary Key without any explicit configuration?
+    // Entity Framework follows a convention called "Key Discovery".
+    // Any property named "Id" or "<ClassName>Id" is automatically treated as the Primary Key.
+    // Since this class is named Book and it contains a property named Id,
+    // Entity Framework automatically recognizes it as the table's primary key.
+
+    // Q2: Why is "Country" nullable in the database while "Price" is not?
+    // Because of the data types used in the classes.
+    // In C#, "string?" means the value is nullable, so EF maps it to a nullable column in the database.
+    // "decimal" is a non-nullable value type, so EF creates the database column as NOT NULL.
+    // If it were written as "decimal?" it would become nullable in the database.
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTime? PublishedDate { get; set; }
+    }
+}
