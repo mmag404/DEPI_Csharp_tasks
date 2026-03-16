32c32cb9b741dd2fee825e7a1da3d4c5bb039651
magdy <mmag8055@gmail.com>
2026-03-17 01:45:13 +0200

    done

---
+++ task-13/day02/Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using day02.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace day02.Controllers
+{
+    public class StudentController : Controller
+    {
+        StudentBL studentBL = new StudentBL();
+
+        public IActionResult ShowAll()
+        {
+            var students = studentBL.GetAll();
+            return View(students);
+        }
+
+        public IActionResult ShowDetails(int id)
+        {
+            var student = studentBL.GetById(id);
+
+            if (student == null)
+                return NotFound();
+
+            return View(student);
+        }
+    }
+}
---
+++ task-13/day02/Models/StudentBL.cs
@@ -0,0 +1,19 @@
+﻿using day02.data.Contexts;
+
+namespace day02.Models
+{
+    public class StudentBL
+    {
+        UniversityDBContext context = new UniversityDBContext();
+
+        public List<Student> GetAll()
+        {
+            return context.Students.ToList();
+        }
+
+        public Student GetById(int id)
+        {
+            return context.Students.FirstOrDefault(s => s.Id == id);
+        }
+    }
+}
