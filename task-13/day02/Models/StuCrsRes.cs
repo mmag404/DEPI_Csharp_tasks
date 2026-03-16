0beb13403025fe67da62d74c7f464b1167326168
magdy <mmag8055@gmail.com>
2026-03-16 23:59:12 +0200

    done

---
+++ task-13/day02/Models/StuCrsRes.cs
@@ -0,0 +1,13 @@
+﻿namespace day02.Models
+{
+    public class StuCrsRes
+    {
+        public int StudentId { get; set; }
+        public int CourseId { get; set; }
+
+        public int Grade { get; set; }
+
+        public Student Student { get; set; }
+        public Course Course { get; set; }
+    }
+}
---
+++ task-13/day02/data/Configurations/StuCrsResConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using day02.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace day02.data.Configurations
+{
+    public class StuCrsResConfiguration : IEntityTypeConfiguration<StuCrsRes>
+    {
+        public void Configure(EntityTypeBuilder<StuCrsRes> builder)
+        {
+            builder.HasKey(x => new { x.StudentId, x.CourseId });
+
+            builder.HasOne(x => x.Student)
+                   .WithMany(s => s.StuCrsResults)
+                   .HasForeignKey(x => x.StudentId)
+                   .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(x => x.Course)
+                   .WithMany(c => c.StuCrsResults)
+                   .HasForeignKey(x => x.CourseId)
+                   .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
