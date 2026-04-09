43b98f300344669359c604ed197cdbb04d43938d
magdy <mmag8055@gmail.com>
2026-04-09 03:23:54 +0200

    day 05 done

---
+++ Task-16/day 05/data/Configurations/StudentConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using day02.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace day02.data.Configurations
+{
+    public class StudentConfiguration : IEntityTypeConfiguration<Student>
+    {
+        public void Configure(EntityTypeBuilder<Student> builder)
+        {
+            builder.HasKey(s => s.Id);
+
+            builder.Property(s => s.Name)
+                   .IsRequired()
+                   .HasMaxLength(100);
+
+            builder.HasOne(s => s.Department)
+                   .WithMany(d => d.Students)
+                   .HasForeignKey(s => s.DepartmentId)
+                   .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
